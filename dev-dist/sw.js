876938c89eb1f32a416c1f490ceb8b0a8a44db3b
Paulo Conde <pauloconde@gmail.com>
2026-05-10 17:19:17 -0400

    fix: drawer bg color & logo drawer setting

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.eguocdalof"
+    "revision": "0.qlbuvto1gj"
   }], {
     "directoryIndex": "index.html"
   });
